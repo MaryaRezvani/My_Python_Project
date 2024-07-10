425b216ca2f82afe9d706b5913c2fa3fe33a553c
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-07-10 18:31:30 +0330

    Removed folder src

--- src/game_logic/hint_generator.py
+++
@@ -1,6 +0,0 @@
-def provide_hint(guess, actual_number):
-    if guess < actual_number:
-        print('your guess is too low.')
-    elif guess > actual_number:
-        print('your guess is too high.')
-            
--- src/game_logic/number_generator.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def generate_number_random(start:int, end:int) -> int:
-    """Generate a random number between start and end"""
-    return random.randint(start, end)
